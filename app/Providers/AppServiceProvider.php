13838263db3aae58b5fea9817c040fd4e83076f3
Ggero Mpyangu <nathanmpyangu@users.noreply.github.com>
2019-07-30 15:16:45 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //added
+        //added today
         Schema::defaultStringLength(191);
     }
 
