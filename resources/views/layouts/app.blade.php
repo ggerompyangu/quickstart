b6ed887036469afd3fe7875715132aed1c79a902
Ggero Mpyangu <nathanmpyangu@users.noreply.github.com>
2019-08-02 12:02:41 +0300

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Laravel Quickstart - Basic</title>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<!-- Navbar contents -->
+		</nav>
+	</div>
+
+	@yield('content')
+	
+</body>
+</html>
